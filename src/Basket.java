68ec455c990e81f0661b4d87d4e87f2688162bb0
Ilyailyou <yaaponia@gmail.com>
2022-09-24 21:34:33 +0500

    Initial commit

---
+++ src/Basket.java
@@ -0,0 +1,64 @@
+import java.io.File;
+import java.io.*;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Basket {
+    private String[] names;
+    private int[] prices;
+    private int[] count;
+
+    public Basket(String[] names, int[] prices) {
+        this.names = names;
+        this.prices = prices;
+        this.count = new int[names.length];
+    }
+    public Basket(String[] names, int[] prices, int[] count) {
+        this.names = names;
+        this.prices = prices;
+        this.count = count;
+    }
+    public String[] getNames() {
+        return names;
+    }
+    public int[] getPrices() {
+        return prices;
+    }
+
+    public int[] getCount() {
+        return count;
+    }
+    public void addToCart(int productNum, int amount) {
+        this.count[productNum] += amount;
+        if(count[productNum] < 0) {
+            this.count[productNum] = 0;
+        }
+    }
+    public void printCart() {
+        int sum = 0;
+        System.out.println("Ваша корзина:");
+        for (int i = 0; i < names.length; i++) {
+            if(count[i] != 0) {
+                System.out.println((i+1)+". "+names[i]+" ("+count[i]+"шт.),всего "+(count[i]*prices[i])+" рублей");
+                sum += count[i] * prices[i];
+            }
+        }
+        System.out.println("Итого:"+sum+" рублей.");
+    }
+
+    public void saveTxt(File textFile) throws IOException {
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(textFile))) {
+            writer.write(String.join(" ", names) + "\n");
+            String pricesTxt = Arrays.stream(prices)
+                    .mapToObj(String::valueOf)
+                    .collect(Collectors.joining(" "));
+            writer.write(pricesTxt+"\n");
+            String countTxt = Arrays.stream(count)
+                    .mapToObj(String::valueOf)
+                    .collect(Collectors.joining(" "));
+            writer.write(countTxt+"\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        File file = new File("Basket.txt");
+        Basket basket = loadFromTxtFile(file);
+        if(basket == null) {
+            basket = new Basket(new String[] {"Хлеб", "Яблоки", "Молоко"}, new int[]{100, 200, 300});
+        }
+        String[] products = basket.getNames();
+        int[] prices = basket.getPrices();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Список возможных товаров для покупки");
+        for (int i = 0; i < products.length; i++) {
+            System.out.println((i + 1) + "." + " " + products[i] + " " + prices[i] + " " + "руб/шт");
+        }
+
+        while (true) {
+            int productNumber = 0;
+            int productCount = 0;
+
+            System.out.println("Выберите товар и количество или введите 'end' ");
+            String input = scanner.nextLine();
+            if ("end".equals(input)) {
+                break;
+            }
+            String[] amount = input.split(" ");
+            productNumber = Integer.parseInt(amount[0]) - 1;
+            productCount = Integer.parseInt(amount[1]);
+            basket.addToCart(productNumber, productCount);
+            basket.saveTxt(file);
+            basket.printCart();
+
+        }
+        basket.printCart();
+    }
+    public static Basket loadFromTxtFile(File textFile) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(textFile))) {
+            String[] names = reader.readLine().split(" ");
+            int[] prices = Arrays.stream(reader.readLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+            int[] count = Arrays.stream(reader.readLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+            return new Basket(names, prices, count);
+        } catch (FileNotFoundException e) {
+            return null;
+        } catch (IOException e) {
+            return null;
+        }
+    }
+}
+
