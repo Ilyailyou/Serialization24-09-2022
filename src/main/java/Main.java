316412371a413b691a0dc7f037c2579e338fc399
Ilyailyou <yaaponia@gmail.com>
2022-10-03 22:32:18 +0500

    Config added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,14 @@
 
 import org.json.simple.parser.ParseException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
@@ -8,10 +16,30 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, ParseException {
-        File file = new File("Basket.json");
-        File logFile = new File("log.csv");
-        Basket basket = Basket.loadFromJson(file);
+    public static boolean loadEnabled = false;
+    public static String loadFileName;
+    public static String loadFormat;
+    public static boolean saveEnabled = true;
+    public static String saveFileName = "Basket.json";
+    public static String saveFormat = "json";
+    public static boolean logEnabled = true;
+    public static String logFileName = "Client.csv";
+
+    public static void main(String[] args) throws IOException, ParseException, ParserConfigurationException, SAXException {
+
+        loadConfig();
+
+
+        Basket basket = null;
+        if(loadEnabled) {
+            File file = new File(loadFileName);
+            if(loadFormat.equals("json")) {
+                basket.loadFromJson(file);
+            } else if (loadFormat.equals("txt")) {
+                basket.loadFromTxtFile(file);
+            }
+        }
+
         if (basket == null) {
             basket = new Basket(new String[]{"Хлеб", "Яблоки", "Молоко"}, new int[]{100, 200, 300});
         }
@@ -39,12 +67,67 @@ public class Main {
             productNumber = Integer.parseInt(amount[0]) - 1;
             productCount = Integer.parseInt(amount[1]);
             basket.addToCart(productNumber, productCount);
-            basket.saveJson(file);
+            if(saveEnabled) {
+                File fileSave = new File(saveFileName);
+                if(saveFormat.equals("json")) {
+                    System.out.println("Гаврюшка ко мне");
+                    basket.saveJson(fileSave);
+                } else if (saveFormat.equals("txt")) {
+                    basket.saveTxt(fileSave);
+                }
+            }
             basket.printCart();
             clientLog.log(productNumber + 1, productCount);
         }
         basket.printCart();
-        clientLog.exportAsCSV(logFile);
+        if(logEnabled) {
+            File logFile = new File(logFileName);
+            clientLog.exportAsCSV(logFile);
+        }
+    }
+    public static void loadConfig() throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse("shop.xml");
+        Node root = doc.getDocumentElement();
+        Element rootElement = (Element) root;
+
+        NodeList list = root.getChildNodes();
+        for (int i = 0; i < list.getLength(); i++) {
+            Node currentNode = list.item(i);
+            if (currentNode.getNodeType() == Node.ELEMENT_NODE) {
+                if (currentNode.getNodeName().equals("load")) {
+                    Element element = (Element) currentNode;
+                    Main.loadEnabled = element.getElementsByTagName("enabled").item(0).getTextContent().equals("true") ? true : false;
+                    Main.loadFileName = element.getElementsByTagName("fileName").item(0).getTextContent();
+                    Main.loadFormat = element.getElementsByTagName("format").item(0).getTextContent();
+                } else if (currentNode.getNodeName().equals("save")) {
+                    Element element = (Element) currentNode;
+                    Main.saveEnabled = element.getElementsByTagName("enabled").item(0).getTextContent().equals("true") ? true : false;
+                    Main.saveFileName = element.getElementsByTagName("fileName").item(0).getTextContent();
+                    Main.saveFormat = element.getElementsByTagName("format").item(0).getTextContent();
+                } else if (currentNode.getNodeName().equals("log")) {
+                    Element element = (Element) currentNode;
+                    Main.logEnabled = element.getElementsByTagName("enabled").item(0).getTextContent().equals("true") ? true : false;
+                    Main.logFileName = element.getElementsByTagName("fileName").item(0).getTextContent();
+
+                }
+            }
+        }
+        /*Element load = (Element) rootElement.getElementsByTagName("load");
+        Main.loadEnabled = load.getElementsByTagName("enabled").item(0).getTextContent().equals("true");
+        Main.loadFileName = load.getElementsByTagName("fileName").item(0).getTextContent();
+        Main.loadFormat = load.getElementsByTagName("format").item(0).getTextContent();
+
+        Element save = (Element) rootElement.getElementsByTagName("save");
+        Main.saveEnabled = save.getElementsByTagName("enabled").item(0).getTextContent().equals("true");
+        Main.saveFileName = save.getElementsByTagName("fileName").item(0).getTextContent();
+        Main.saveFormat = save.getElementsByTagName("format").item(0).getTextContent();
+
+        Element log = (Element) rootElement.getElementsByTagName("log");
+        Main.logEnabled = log.getElementsByTagName("enabled").item(0).getTextContent().equals("true");
+        Main.logFileName = log.getElementsByTagName("fileName").item(0).getTextContent();*/
+
     }
 }
 
